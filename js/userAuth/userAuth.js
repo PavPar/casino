171cc1ba39b41011827e8b9002b876837d98a1b5
PavPar <ParamonPav@yandex.ru>
2020-08-16 22:39:44 +0300

    feat: Add style to userAuth.html

---
+++ js/userAuth/userAuth.js
@@ -0,0 +1,32 @@
+const formHiddenClass = "form_visibility-hidden";
+
+const pairLog = {
+    btn: document.querySelector(".button_type-login"),
+    form: document.forms.formLog
+}
+
+const pairReg = {
+    btn: document.querySelector(".button_type-register"),
+    form: document.forms.formReg
+}
+
+function hideForm({ btn, form }) {
+    btn.disabled = false;
+    form.classList.add(formHiddenClass);
+}
+
+function showForm({ btn, form }) {
+    btn.disabled = true;
+    form.classList.remove(formHiddenClass);
+}
+
+pairLog.btn.addEventListener('click', () => {
+    showForm(pairLog);
+    hideForm(pairReg);
+})
+
+
+pairReg.btn.addEventListener('click', () => {
+    showForm(pairReg);
+    hideForm(pairLog);
+})
